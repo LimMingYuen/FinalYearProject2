39b453ac5e650b60b117e702ba93e343672be875
LimMingYuen <89985930+LimMingYuen@users.noreply.github.com>
2021-09-02 21:49:05 +0800

    Add files via upload

---
+++ apps/home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def app():
+    st.title('Home')
+
+    st.write('This is the homepage of the application.')
+
+    st.write('In this app, an application use to predict the performance of student and also applied statiscal technique on the data.')
